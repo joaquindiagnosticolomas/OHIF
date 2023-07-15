90362fcd8ebd71b06f6282b288a3ef1c5fce5d54
joaquindiagnosticolomas <joaquin.diagnosticolomas@gmail.com>
2023-07-15 10:44:18 -0700

    3

--- .netlify/build-deploy-preview.sh
+++ .netlify/build-deploy-preview.sh
@@ -12,7 +12,21 @@ yarn -v
 node -v
 
 # Build && Move PWA Output
-yarn run dev
+yarn run build:ci
 mkdir -p ./.netlify/www/pwa
-mv ./* .netlify/www/pwa -v
+mv platform/viewer/dist/* .netlify/www/pwa -v
 echo 'Web application built and copied'
+
+# Build && Move Docusaurus Output (for the docs themselves)
+cd platform/docs
+yarn install
+yarn run build
+cd ../..
+mkdir -p ./.netlify/www/docs
+mv platform/docs/build/* .netlify/www/docs -v
+echo 'Docs built (docusaurus) and copied'
+
+# Cache all of the node_module dependencies in
+# extensions, modules, and platform packages
+yarn run lerna:cache
+echo 'Nothing left to see here. Go home, folks.'
