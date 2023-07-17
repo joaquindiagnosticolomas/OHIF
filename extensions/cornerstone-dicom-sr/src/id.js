3993d0dfc1963b2ea64eb9ef021ede8df2eeb745
joaquindiagnosticolomas <joaquin.diagnosticolomas@gmail.com>
2023-07-17 15:04:45 -0700

    2

--- extensions/cornerstone-dicom-sr/src/id.js
+++
@@ -1,8 +0,0 @@
-import packageJson from '../package.json';
-
-const id = packageJson.name;
-
-const SOPClassHandlerName = 'dicom-sr';
-const SOPClassHandlerId = `${id}.sopClassHandlerModule.${SOPClassHandlerName}`;
-
-export { SOPClassHandlerName, SOPClassHandlerId, id };
--- extensions/cornerstone/src/types/index.ts
+++
@@ -1,7 +0,0 @@
-import * as CornerstoneCacheService from './CornerstoneCacheService'
-import CornerstoneServices from './CornerstoneServices';
-
-export type {
-  CornerstoneCacheService,
-  CornerstoneServices,
-}
